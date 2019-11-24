2b7a5d36b348243dabcb5c6fd5496caf8cbcfc7f
mrwolffang <mrwolffang@gmail.com>
2019-11-24 10:35:15 +0700

    Update install_rdp.sh

--- install_rdp.sh
+++ install_rdp.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+wget http://download912.mediafire.com/n8vqycm6xmkg/4vr6hu3h0ubcaq2/HitLeap+Viewer.tar.xz
+tar xf HitLeap+Viewer.tar.xz
 apt-get update
 apt-get upgrade
 apt-get install xorg xrdp lxde firefox
