1e43eef843daee6d614b764b6e75a69ffcec1f8e
mrwolffang <mrwolffang@gmail.com>
2019-11-24 11:59:17 +0700

    Update install_rdp.sh

--- install_rdp.sh
+++ install_rdp.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-wget http://download912.mediafire.com/n8vqycm6xmkg/4vr6hu3h0ubcaq2/HitLeap+Viewer.tar.xz
-tar xf HitLeap+Viewer.tar.xz
 apt-get update
 apt-get upgrade
 apt-get install xorg xrdp lxde firefox vuze deluge
-sh -c '"$(dirname "$*")"/app/HitLeap-Viewer' dummy %k
